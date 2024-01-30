df6b30627ab11e5c19e881a0662eda43a77ecf82
james.white24 <jwhite5013@gmail.com>
2024-01-30 11:29:59 -0600

    Start of Yatzee Project

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        JFrame frame = new JFrame();
+    frame.setSize(1000,1000);
+GridLayout card = new GridLayout(5,3);
+    JPanel panel = new JPanel();
+    panel.setLayout(card);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    JLabel label = new JLabel("Hello");
+    JButton roll = new JButton("Roll");
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+    panel.add(label);
+    panel.add(roll);
+    frame.add(panel);
+    frame.setVisible(true);
+
     }
 }
