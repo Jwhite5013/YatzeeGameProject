d26ae2bcc6873d1689baf55bdbef670656810756
james.white24 <jwhite5013@gmail.com>
2024-04-03 11:32:15 -0500

    Started Ai and finished a little

--- src/AI.java
+++ src/AI.java
@@ -1,6 +1,9 @@
+import javax.imageio.plugins.tiff.TIFFImageReadParam;
+import java.awt.*;
+
 public class AI {
     private int[] scores;
-    private boolean[] scorePlayed;
+    static private boolean[] scorePlayed;
     public AI(){
         scores = new int[14];
         scorePlayed = new boolean[14];
@@ -11,11 +14,66 @@ public class AI {
     }
     public static int calculateScore(int[] dice) {
         int score = 0;
-        if (isThreeOfAKind(dice)) {
+        if(shouldKeepForTopSection(dice,1,0)){
+            score = sumOfDiceS(dice,1);
+        }
+       else if(shouldKeepForTopSection(dice,2,1)){
+            score = sumOfDiceS(dice,2);
+        }
+       else if(shouldKeepForTopSection(dice,3,2)){
+            score = sumOfDiceS(dice,3);
+        }
+       else if(shouldKeepForTopSection(dice, 4, 3)){
+           score = sumOfDiceS(dice,4);
+        }
+       else if(shouldKeepForTopSection(dice, 5,4)){
+           score = sumOfDiceS(dice,5);
+        }
+       else if(shouldKeepForTopSection(dice, 6, 5)){
+           score = sumOfDiceS(dice,6);
+        }
+       else if (isThreeOfAKind(dice)) {
             score = sumOfDice(dice);
         }
-        if(isFourOfAKind(dice)){
+       else if(isFourOfAKind(dice)){
             score = sumOfDice(dice);
+        }
+       else if(isFullHouse(dice)){
+
+        }
+       else if(shouldKeepForStraight(dice)){
+            int ones = 0;
+            int twos = 0;
+            int threes = 0;
+            int fours = 0;
+            int fives = 0;
+            int sixes = 0;
+            for (int count : dice) {
+                if (count == 1) {
+                    ones++;
+                }
+                if (count == 2) {
+                    twos++;
+                }
+                if (count == 3) {
+                    threes++;
+                }
+                if (count == 4) {
+                    fours++;
+                }
+                if (count == 5) {
+                    fives++;
+                }
+                if (count == 6) {
+                    sixes++;
+                }
+            }
+            if(ones>=1&&twos>=1&&threes>=1&&fours>=1||twos>=1&&threes>=1&&fours>=1&&fives>=1||threes>=1&&fours>=1&&fives>=1&&sixes>=1){
+               score = 30;
+            }
+            else if(ones==1&&twos==1&&threes==1&&fours==1&&fives==1||twos==1&&threes==1&&fours==1&&fives==1&&sixes==1){
+                score = 40;
+            }
         }
         return score;
     }
@@ -29,7 +87,7 @@ public class AI {
                     count++;
                 }
             }
-            if (count >= 3) {
+            if (count >= 3&&scorePlayed[6]==false) {
                 return true;
             }
         }
@@ -43,20 +101,20 @@ public static boolean isFourOfAKind(int[]dice){
                     count++;
                 }
             }
-            if(count >=4){
+            if(count >=4&&scorePlayed[7]==false){
                 return true;
             }
         }
         return false;
 }
-public static boolean shouldKeepForTopSection(int[]dice,int value){
+public static boolean shouldKeepForTopSection(int[]dice,int value,int v){
         int count = 0;
         for(int die : dice){
             if(die==value){
                 count++;
             }
         }
-        if(count>=value){
+        if(count>=2&&scorePlayed[v]==false){
             return true;
         }
         return false;
@@ -69,7 +127,53 @@ public static boolean shouldKeepForTopSection(int[]dice,int value){
         }
         return sum;
     }
-
+public static int sumOfDiceS(int[] d,int v){
+        int sum = 0;
+        for(int die : d){
+            if(die==v){
+                sum+=die;
+            }
+        }
+        return sum;
+}
+public static boolean shouldKeepForStraight(int[] dice) {
+    int ones = 0;
+    int twos = 0;
+    int threes = 0;
+    int fours = 0;
+    int fives = 0;
+    int sixes = 0;
+    for (int count : dice) {
+        if (count == 1) {
+            ones++;
+        }
+        if (count == 2) {
+            twos++;
+        }
+        if (count == 3) {
+            threes++;
+        }
+        if (count == 4) {
+            fours++;
+        }
+        if (count == 5) {
+            fives++;
+        }
+        if (count == 6) {
+            sixes++;
+        }
+    }
+    if (ones >= 1 && twos >= 1 && threes >= 1 || ones >= 1 && threes >= 1 && fours >= 1||ones>=1&&fours>=1&&fives>=1||twos>=1&&threes>=1&&fours>=1||twos>=1&&fours>=1&&fives>=1||twos>=1&&fives>=1&&sixes>=1||threes>=1&&fours>=1&&fives>=1||threes>=1&&fives>=1&&sixes>=1||fours>=1&&fives>=1&&sixes>=1) {
+    return true;
+    }
+    return false;
+}
+public static boolean shouldKeepForYahztee(int[] d){
+        if(d[0]==d[1]&&d[1]==d[2]&&d[2]==d[3]&&d[4]==d[4]){
+            return true;
+        }
+        return false;
+}
     // Utility method to convert dice array to a string for printing
     public static String diceToString(int[] dice) {
         StringBuilder sb = new StringBuilder();
